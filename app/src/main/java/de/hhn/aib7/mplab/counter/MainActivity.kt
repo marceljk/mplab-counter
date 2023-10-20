88dcc3795a3a7c633a580ce191fd4b2fc533c9b3
Marcel Jacek <marcel.jacek@gmail.com>
2023-10-20 15:59:40 +0200

    Initial commit

---
+++ app/src/main/java/de/hhn/aib7/mplab/counter/MainActivity.kt
@@ -0,0 +1,60 @@
+package de.hhn.aib7.mplab.counter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import de.hhn.aib7.mplab.counter.ui.theme.CounterTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CounterTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    var counter by remember { mutableStateOf(0) }
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.SpaceAround
+                    ) {
+                        Text(text = counter.toString(), style = MaterialTheme.typography.displayLarge )
+                        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceAround) {
+                            Button(onClick = { --counter }, modifier = Modifier.width(150.dp), content = { Text(text = "-", style = MaterialTheme.typography.displayMedium)})
+                            Button(onClick = { ++counter }, modifier = Modifier.width(150.dp), content = { Text(text = "+", style = MaterialTheme.typography.displayMedium)})
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CounterTheme {
+    }
+}
