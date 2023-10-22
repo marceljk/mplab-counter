0369cffc544a19de7cb2491d9a60f7432fecae18
wal6ed <waleed6.kahale@gmail.com>
2023-10-22 21:52:25 +0200

    - add remember state Management - add Button to go/return from/to new Activity

---
+++ app/src/main/java/de/hhn/aib7/mplab/counter/EndActivity.kt
@@ -0,0 +1,71 @@
+package de.hhn.aib7.mplab.counter
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import de.hhn.aib7.mplab.counter.ui.theme.CounterTheme
+
+class EndActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CounterTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.SpaceAround
+                    ) {
+                        val activity = (LocalContext.current as? Activity)
+                        Button(onClick = {
+                            activity?.finish()
+                        },
+                            modifier = Modifier.width(300.dp),
+                            content = {
+                                Text(
+                                    text = "comeback",
+                                    style = MaterialTheme.typography.displayMedium
+                                )
+                            }
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview2() {
+    CounterTheme {
+        Greeting("Android")
+    }
+}
--- app/src/main/java/de/hhn/aib7/mplab/counter/MainActivity.kt
+++ app/src/main/java/de/hhn/aib7/mplab/counter/MainActivity.kt
@@ -1,5 +1,7 @@
 package de.hhn.aib7.mplab.counter
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -20,31 +23,78 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import de.hhn.aib7.mplab.counter.ui.theme.CounterTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            var counter by remember { mutableStateOf(0) }
             CounterTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    var counter by remember { mutableStateOf(0) }
                     Column(
                         modifier = Modifier.fillMaxSize(),
                         horizontalAlignment = Alignment.CenterHorizontally,
                         verticalArrangement = Arrangement.SpaceAround
                     ) {
-                        Text(text = counter.toString(), style = MaterialTheme.typography.displayLarge )
-                        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceAround) {
-                            Button(onClick = { --counter }, modifier = Modifier.width(150.dp), content = { Text(text = "-", style = MaterialTheme.typography.displayMedium)})
-                            Button(onClick = { ++counter }, modifier = Modifier.width(150.dp), content = { Text(text = "+", style = MaterialTheme.typography.displayMedium)})
+                        Text(
+                            text = counter.toString(),
+                            style = MaterialTheme.typography.displayLarge
+                        )
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceAround
+                        ) {
+                            Button(
+                                onClick = { counter-- },
+                                modifier = Modifier.width(150.dp),
+                                content = {
+                                    Text(
+                                        text = "-",
+                                        style = MaterialTheme.typography.displayMedium
+                                    )
+                                }
+                            )
+                            Button(
+                                onClick = { counter++ },
+                                modifier = Modifier.width(150.dp),
+                                content = {
+                                    Text(
+                                        text = "+",
+                                        style = MaterialTheme.typography.displayMedium
+                                    )
+                                }
+                            )
                         }
+                        /*Column(modifier = Modifier.padding(16.dp)) {
+                            Text(
+                                text = "Hello, $name",
+                                modifier = Modifier.padding(bottom = 8.dp),
+                                style = MaterialTheme.typography.bodyMedium
+                            )
+                            OutlinedTextField(value = name, onValueChange = {name = it}, label = { Text("Name") })
+                        }*/
+                        val activity = (LocalContext.current as? Activity)
+                        Button(
+                            onClick = {
+                                activity?.startActivity(Intent(this@MainActivity, EndActivity::class.java))
+                            },
+                            modifier = Modifier.width(300.dp),
+                            content = {
+                                Text(
+                                    text = "next screen",
+                                    style = MaterialTheme.typography.displayMedium
+                                )
+                            }
+                        )
                     }
                 }
             }
