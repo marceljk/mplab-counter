19893af8c6661bb70bd88edb28b87d68e1db3b78
wal6ed <waleed6.kahale@gmail.com>
2023-12-04 18:56:24 +0100

    last push

--- app/src/main/java/de/hhn/aib7/mplab/counter/MainActivity.kt
+++ app/src/main/java/de/hhn/aib7/mplab/counter/MainActivity.kt
@@ -1,7 +1,5 @@
 package de.hhn.aib7.mplab.counter
 
-import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -12,28 +10,27 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import de.hhn.aib7.mplab.counter.ui.theme.CounterTheme
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            var counter by remember { mutableStateOf(0) }
+            var counterRememberSaveAble by rememberSaveable { mutableStateOf(0) }
+            var counterRemember by remember { mutableStateOf(0) }
             CounterTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
@@ -46,15 +43,47 @@ class MainActivity : ComponentActivity() {
                         verticalArrangement = Arrangement.SpaceAround
                     ) {
                         Text(
-                            text = counter.toString(),
-                            style = MaterialTheme.typography.displayLarge
+                            text = "Counter",
+                            style = MaterialTheme.typography.displayLarge,
                         )
+                        Column(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.SpaceAround
+                        ) {
+                            Text(
+                                text = "RememberSavable:",
+                                style = MaterialTheme.typography.displaySmall
+                            )
+                            Text(
+                                text = counterRememberSaveAble.toString(),
+                                style = MaterialTheme.typography.displaySmall
+                            )
+                        }
+                        Column(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.SpaceAround
+                        ) {
+                            Text(
+                                text = "Remember:",
+                                style = MaterialTheme.typography.displaySmall
+                            )
+                            Text(
+                                text = counterRemember.toString(),
+                                style = MaterialTheme.typography.displaySmall
+                            )
+                        }
                         Row(
                             modifier = Modifier.fillMaxWidth(),
-                            horizontalArrangement = Arrangement.SpaceAround
+                            horizontalArrangement = Arrangement.SpaceAround,
+                            verticalAlignment = Alignment.CenterVertically
                         ) {
                             Button(
-                                onClick = { counter-- },
+                                onClick = {
+                                    counterRemember--
+                                    counterRememberSaveAble--
+                                },
                                 modifier = Modifier.width(150.dp),
                                 content = {
                                     Text(
@@ -64,7 +93,10 @@ class MainActivity : ComponentActivity() {
                                 }
                             )
                             Button(
-                                onClick = { counter++ },
+                                onClick = {
+                                    counterRemember++
+                                    counterRememberSaveAble++
+                                },
                                 modifier = Modifier.width(150.dp),
                                 content = {
                                     Text(
@@ -74,27 +106,6 @@ class MainActivity : ComponentActivity() {
                                 }
                             )
                         }
-                        /*Column(modifier = Modifier.padding(16.dp)) {
-                            Text(
-                                text = "Hello, $name",
-                                modifier = Modifier.padding(bottom = 8.dp),
-                                style = MaterialTheme.typography.bodyMedium
-                            )
-                            OutlinedTextField(value = name, onValueChange = {name = it}, label = { Text("Name") })
-                        }*/
-                        val activity = (LocalContext.current as? Activity)
-                        Button(
-                            onClick = {
-                                activity?.startActivity(Intent(this@MainActivity, EndActivity::class.java))
-                            },
-                            modifier = Modifier.width(300.dp),
-                            content = {
-                                Text(
-                                    text = "next screen",
-                                    style = MaterialTheme.typography.displayMedium
-                                )
-                            }
-                        )
                     }
                 }
             }
@@ -108,3 +119,4 @@ fun GreetingPreview() {
     CounterTheme {
     }
 }
+
